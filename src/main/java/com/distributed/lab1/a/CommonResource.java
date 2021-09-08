27d0d3e854e68c0ddc56b33428a3663a774c69f3
DoctorMatt47 <DoctorMatt47@gmail.com>
2021-09-08 22:40:23 +0300

    lab1.a - Changed main file

--- src/main/java/com/distributed/lab1/a/CommonResource.java
+++ src/main/java/com/distributed/lab1/a/CommonResource.java
@@ -1,17 +1,24 @@
 package com.distributed.lab1.a;
 
+import javax.swing.*;
+
 public class CommonResource {
-    private int value;
+    private final JSlider slider;
+    private Boolean isCompleted = false;
 
-    public CommonResource(int value) {
-        this.value = value;
+    public CommonResource(JSlider sliderToChange) {
+        this.slider = sliderToChange;
     }
 
-    public int getValue() {
-        return value;
+    public JSlider getSlider() {
+        return slider;
     }
 
-    public void setValue(int value) {
-        this.value = value;
+    public Boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void complete() {
+        isCompleted = true;
     }
 }

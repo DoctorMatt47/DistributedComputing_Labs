d5fef0a6340acb6c134a3cc9acefc825d41f71f6
DoctorMatt47 <DoctorMatt47@gmail.com>
2021-09-08 22:40:23 +0300

    lab1.a - Changed main file

--- src/main/java/com/distributed/lab1/a/Main.java
+++ src/main/java/com/distributed/lab1/a/Main.java
@@ -1,8 +1,13 @@
 package com.distributed.lab1.a;
 
-public class Main
-{
+public class Main {
     public static void main(String[] args) {
-
+        var resToChange = new CommonResource(50);
+        var resChangeTo1 = new CommonResource(10);
+        var resChangeTo2 = new CommonResource(90);
+        var thread1 = new Thread(new CommonResourceGraduallyChanger(resToChange, resChangeTo1, 200));
+        var thread2 = new Thread(new CommonResourceGraduallyChanger(resToChange, resChangeTo2, 50));
+        thread1.start();
+        thread2.start();
     }
 }

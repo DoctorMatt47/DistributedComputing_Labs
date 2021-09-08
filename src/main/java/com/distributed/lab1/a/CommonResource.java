78d3b683b7675e058ed32a5c63634705bf4d558a
DoctorMatt47 <DoctorMatt47@gmail.com>
2021-09-08 22:40:08 +0300

    lab1.a - Added common resource and common resource changer

---
+++ src/main/java/com/distributed/lab1/a/CommonResource.java
@@ -0,0 +1,17 @@
+package com.distributed.lab1.a;
+
+public class CommonResource {
+    private int value;
+
+    public CommonResource(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}

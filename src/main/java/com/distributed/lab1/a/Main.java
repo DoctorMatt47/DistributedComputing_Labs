af86ee84037c6a81d15369fb64ff0f5efee81000
DoctorMatt47 <DoctorMatt47@gmail.com>
2021-09-09 16:33:10 +0300

    lab1.a - Changed main

--- src/main/java/com/distributed/lab1/a/Main.java
+++ src/main/java/com/distributed/lab1/a/Main.java
@@ -1,13 +1,12 @@
 package com.distributed.lab1.a;
 
 public class Main {
+
     public static void main(String[] args) {
-        var resToChange = new CommonResource(50);
-        var resChangeTo1 = new CommonResource(10);
-        var resChangeTo2 = new CommonResource(90);
-        var thread1 = new Thread(new CommonResourceGraduallyChanger(resToChange, resChangeTo1, 200));
-        var thread2 = new Thread(new CommonResourceGraduallyChanger(resToChange, resChangeTo2, 50));
-        thread1.start();
-        thread2.start();
+        var gui = new Gui();
+
+        var commonResources = new CommonResource(gui.getSlider());
+        new Thread(new SliderValueChanger(commonResources, 10, 100)).start();
+        new Thread(new SliderValueChanger(commonResources, 90, 200)).start();
     }
 }
